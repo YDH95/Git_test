94fa8b1744f406e7655464e8d9e303afe071a3b5
YDH95 <wowhky81234@gmail.com>
2022-08-08 15:01:25 +0900

    코드워

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,27 @@
 
 public class test {
 
-	public static void main(String[] args) {
-		System.out.println("hello");
-
+	public static String accum(String s) {
+		System.out.println(s);
+		return null;
 	}
+
+	public static void main(String[] args) {
+//		Code_war code = new Code_war();
+		accum("ZpglnRxqenU");
+	}
+	/*
+	 * testing(Accumul.accum("ZpglnRxqenU"),
+	 * "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+	 * ); testing(Accumul.accum("NyffsGeyylB"),
+	 * "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+	 * ); testing(Accumul.accum("MjtkuBovqrU"),
+	 * "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+	 * ); testing(Accumul.accum("EvidjUnokmM"),
+	 * "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+	 * ); testing(Accumul.accum("HbideVbxncC"),
+	 * "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+	 * );
+	 */
 
 }
