ca48dfcacfbf058733e4c371bfd152dbfaba7647
YDH95 <wowhky81234@gmail.com>
2022-08-10 14:29:30 +0900

    test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,38 +1,57 @@
-import java.util.Arrays;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class test {
 
-	public static String accum(String s) {
-		String[] arr = s.split("");
-		String str = "";
+	public static String getMiddle(String word) {
+		int x = 0;
+		String[] str = word.split("");
+		String answer = "";
 
-		for (int i = 0; i < arr.length; i++) {
-			str += arr[i].toUpperCase();
-			for (int j = 0; j < i; j++) {
-				str += arr[i].toLowerCase();
-			}
-			str += "-";
+		if (str.length % 2 == 0) {
+			x = str.length / 2 - 1;
+			answer = str[x] + "" + str[x + 1];
+		} else {
+			x = str.length / 2;
+			answer = str[x];
 		}
-		str = str.substring(0, str.length() - 1);
-		return str;
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
 //		Code_war code = new Code_war();
-		accum("ZpglnRxqenU");
+//		getMiddle("midle");
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			Date date1 = sdf.parse("12:27:00");
+			System.out.println(date1.getTime());
+			
+			Date nowDate = new Date();
+			String date = nowDate.toString().substring(11, 19);
+			System.out.println(date);
+			long date2 = sdf.parse(date).getTime();
+			System.out.println((date2 - date1.getTime()) / 1000);
+			System.out.println((date2 - date1.getTime()) / (1000 * 60));
+			System.out.println((date2 - date1.getTime()) / (1000 * 60 * 60));
+/*
+ * 초 : / 1000 
+분 : / (1000 * 60)
+시 : / (1000 * 60 * 60)*/			
+			
+			
+//			Calendar cal = Calendar.getInstance();
+//			cal.add(Calendar.MONTH, 1);
+//			String nowYear = new SimpleDateFormat("HH").format(cal.getTime());
+//			String nowMonth = new SimpleDateFormat("mm").format(cal.getTime());
+//			System.out.println(nowYear);
+//			System.out.println(nowMonth);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	/*
-	 * testing(Accumul.accum("ZpglnRxqenU"),
-	 * "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
-	 * ); testing(Accumul.accum("NyffsGeyylB"),
-	 * "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
-	 * ); testing(Accumul.accum("MjtkuBovqrU"),
-	 * "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
-	 * ); testing(Accumul.accum("EvidjUnokmM"),
-	 * "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
-	 * ); testing(Accumul.accum("HbideVbxncC"),
-	 * "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
-	 * );
-	 */
 
 }
